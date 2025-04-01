3aa83f447449e9d534643bbec880f8c3d808a050
John44764 <johnotten08@gmail.com>
2025-04-02 01:21:24 +0200

    Added first delete functionality

--- taxCollector.py
+++ taxCollector.py
@@ -3,29 +3,42 @@ import pandas as pd
 
 def submitUserInfos(table):
     if submitButton and name and schulden:
-        st.session_state.infos["Name"].append(f"{name}")
+        st.session_state.infos["Namen"].append(f"{name}")
         st.session_state.infos["Schulden"].append(f"{schulden}")
 
         return table
     elif submitButton and name:
-        st.error("Error: schulden ist leer")
+        st.error("Error: keine schulden eingetragen")
     elif submitButton and schulden:
-        st.error("Error: name ist leer")
+        st.error("Error: kein name eingetragen")
     elif submitButton:
-        st.error("Error: name und schulden sind leer")
+        st.error("Error: kein name und keine schulden eingetragen")
 
 if "infos" not in st.session_state:
-    st.session_state.infos = {"Name": [], "Schulden": []}
+    st.session_state.infos = {"Namen": [], "Schulden": []}
 
 #table init
 table = pd.DataFrame(st.session_state.infos)
+
 
 with st.form(key="my_form"):
     name = st.text_input("Name")
     schulden = st.text_input("Schulden")
     submitButton = st.form_submit_button("hinzufügen")
-
 submitUserInfos(table)
+
+
+nameOfUserToDelete = st.selectbox("select", st.session_state.infos["Namen"])
+deleteButton = st.button("Delete")
+
+if deleteButton and st.session_state.infos["Namen"]:
+    indexOfnameOfUserToDelete = st.session_state.infos["Namen"].index(nameOfUserToDelete)
+    st.session_state.infos["Namen"].pop(indexOfnameOfUserToDelete)
+    st.session_state.infos["Schulden"].pop(indexOfnameOfUserToDelete)
+elif deleteButton:
+    st.error("Error: can't delete with no entries in the table")
+
+#TODO: update selectbox so u cant delete things that aren't in the list
 
 #table last update before deploy
 table = pd.DataFrame(st.session_state.infos)
