2063c403bb175448afa79c90ed102e12f0fb2133
John44764 <johnotten08@gmail.com>
2025-04-02 00:12:15 +0200

    Small refactor

--- taxCollector.py
+++ taxCollector.py
@@ -1,30 +1,32 @@
 import streamlit as st
 import pandas as pd
 
+def submitUserInfos(table):
+    if submitButton and name and schulden:
+        st.session_state.infos["Name"].append(f"{name}")
+        st.session_state.infos["Schulden"].append(f"{schulden}")
+
+        return table
+    elif submitButton and name:
+        st.error("Error: schulden ist leer")
+    elif submitButton and schulden:
+        st.error("Error: name ist leer")
+    elif submitButton:
+        st.error("Error: name und schulden sind leer")
 
 if "infos" not in st.session_state:
     st.session_state.infos = {"Name": [], "Schulden": []}
 
+#table init
 table = pd.DataFrame(st.session_state.infos)
 
 with st.form(key="my_form"):
-
     name = st.text_input("Name")
     schulden = st.text_input("Schulden")
-
     submitButton = st.form_submit_button("hinzufügen")
 
+submitUserInfos(table)
 
-if submitButton and name and schulden:
-    st.session_state.infos["Name"].append(f"{name}")
-    st.session_state.infos["Schulden"].append(f"{schulden}")
-
-    table = pd.DataFrame(st.session_state.infos)
-elif submitButton and name:
-    st.error("Error: schulden ist leer")
-elif submitButton and schulden:
-    st.error("Error: name ist leer")
-elif submitButton:
-    st.error("Error: name und schulden sind leer")
-
+#table last update before deploy
+table = pd.DataFrame(st.session_state.infos)
 st.write(table)
